b00983fb6aca16d7d2a06720c7e24b9ede4ace91
limunosekai <powerlsh0103@gmail.com>
2021-05-24 16:26:31 +0900

    Add Card, Image Carousel

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,51 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Col, Card, Row, Button } from 'antd';
+import ImageSlider from '../../utils/ImageSlider';
+
+const { Meta } = Card;
 
 function LandingPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.post('/api/product/products').then((response) => {
+      if (response.data.success) {
+        setProducts(response.data.productInfo);
+      } else {
+        alert('상품 데이터 로딩 실패');
+      }
+    });
+  }, []);
+
+  const renderCards = products.map((product, index) => {
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`${product.price} 원`} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: '75%', margin: '3rem auto' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h2>즐거운 쇼핑~😀</h2>
+      </div>
+      {/* Filter */}
+
+      {/* Search */}
+
+      {/* Cards */}
+
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button>더보기</Button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
--- server/routes/product.js
+++ server/routes/product.js
@@ -39,4 +39,14 @@ router.post('/', (req, res) => {
   });
 });
 
+router.post('/products', (req, res) => {
+  // products collection의 상품 정보 load
+  Product.find()
+    .populate('writer')
+    .exec((err, productInfo) => {
+      if (err) return res.status(400).json({ success: false, err });
+      return res.status(200).json({ success: true, productInfo });
+    });
+});
+
 module.exports = router;
