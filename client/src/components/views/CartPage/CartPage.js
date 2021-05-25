61474fa246fd8c2278c1a0ab413716e3455c5ad4
limunosekai <powerlsh0103@gmail.com>
2021-05-25 23:52:33 +0900

    장바구니 총 결제금액 계산

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getCartItems } from '../../../_actions/user_actions';
 import UserCardBlock from './Sections/UserCardBlock';
 
 function CartPage(props) {
   const dispatch = useDispatch();
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     let cartItems = [];
@@ -15,10 +16,23 @@ function CartPage(props) {
         props.user.userData.cart.forEach((item) => {
           cartItems.push(item.id);
         });
-        dispatch(getCartItems(cartItems, props.user.userData.cart));
+        dispatch(getCartItems(cartItems, props.user.userData.cart)).then(
+          (response) => {
+            calculateTotal(response.payload.product);
+          }
+        );
       }
     }
   }, [props.user.userData]);
+
+  let calculateTotal = (cartDetail) => {
+    let amount = 0;
+    cartDetail.map(
+      (item) => (amount += parseInt(item.price, 10) * item.quantity)
+    );
+    setTotal(amount);
+  };
+
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
       <h1>My Cart</h1>
@@ -26,6 +40,11 @@ function CartPage(props) {
         <UserCardBlock
           products={props.user.cartDetail && props.user.cartDetail.product}
         />
+      </div>
+      <div style={{ width: '90%', margin: '3rem auto', textAlign: 'center' }}>
+        <h2>
+          결제하실 총 금액:<strong> {total} 원</strong>
+        </h2>
       </div>
     </div>
   );
