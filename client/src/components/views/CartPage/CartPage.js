0d54b051f099957451b35fb742382063ed578e85
limunosekai <powerlsh0103@gmail.com>
2021-05-27 01:02:42 +0900

    Add Paypal

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { getCartItems, removeCartItem } from '../../../_actions/user_actions';
 import UserCardBlock from './Sections/UserCardBlock';
 import { Empty } from 'antd';
+import Paypal from '../../utils/Paypal';
 
 function CartPage(props) {
   const dispatch = useDispatch();
@@ -65,6 +66,7 @@ function CartPage(props) {
           <Empty description={false} />
         </>
       )}
+      {showTotal && <Paypal total={total} />}
     </div>
   );
 }
